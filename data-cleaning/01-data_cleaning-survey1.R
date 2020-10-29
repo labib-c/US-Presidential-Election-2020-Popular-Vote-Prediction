fe643dfb88a341fdf5705ef43a8b69116fb6cb5a
saksham a <sakshamahluwalia>
2020-10-28 21:51:59 -0400

    added vote trump col

--- data-cleaning/01-data_cleaning-survey1.R
+++ data-cleaning/01-data_cleaning-survey1.R
@@ -16,7 +16,8 @@ library(tidyverse)
 
 
 #LABIBS WD
-WD = "/Users/labibchowdhury/coursework/STA304/ps3/US-Presidential-Election-2020-Popular-Vote-Prediction"
+
+#WD = "/Users/labibchowdhury/coursework/STA304/ps3/US-Presidential-Election-2020-Popular-Vote-Prediction"
 
 #ERICS WD
 # WD = 
@@ -25,11 +26,11 @@ WD = "/Users/labibchowdhury/coursework/STA304/ps3/US-Presidential-Election-2020-
 #WD =
 
 #SAKSHAMS WD
-#WD =
+WD = "C:/Users/saksh/Desktop/STA304/Pset3"
 
 setwd(WD)
 # Read in the raw data (You might need to change this if you use a different dataset)
-raw_data <- read_dta("inputs/ns20200625/ns20200625.dta")
+raw_data <- read_dta("Data/ns20200625.dta")
 # Add the labels
 raw_data <- labelled::to_factor(raw_data)
 # Just keep some variables
@@ -61,8 +62,9 @@ reduced_data <-
 
 reduced_data<-
   reduced_data %>%
-  mutate(vote_trump = 
-           ifelse(vote_2020=="Donald Trump", 1, 0))
+  mutate(vote_trump =
+           ifelse(vote_2020=="Donald Trump", 1, ifelse(vote_2020=="Joe Biden",0,-1))) %>%
+  filter(vote_trump != -1)
 
 # Saving the survey/sample data as a csv file in my
 # working directory
